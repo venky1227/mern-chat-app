70a71f5ad8a55d7cc76f94fb0b9203b3a8f08c3d
venky1227 <46067605+venky1227@users.noreply.github.com>
2024-11-13 12:05:17 +0530

    1st part completed - backend

---
+++ Backend/models/user.model.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+   fullName: {
+      type: 'string',
+      required: true
+   },
+   username: {
+      type: 'string',
+      required: true,
+      unique: true
+   },
+   password: {
+      type: 'string',
+      required: true,
+      minlength: 6
+   },
+   gender: {
+      type: 'string',
+      required: true,
+      enum: ['male', 'female']
+   },
+   profilePic: {
+      type: 'string',
+      default: "",
+   },
+   //   createdAt,updatedAt => Members since <createdAt>
+},
+   { timestamps: true });
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
