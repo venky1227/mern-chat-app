7e8a4cb60d4991bd6e64ffa5b200e7d3dcb9665d
venky1227 <46067605+venky1227@users.noreply.github.com>
2024-11-17 19:59:27 +0530

    3rd part completed - connecting frontend and backend

--- Frontend/src/components/Conversations.jsx
+++ Frontend/src/components/Conversations.jsx
@@ -1,23 +1,26 @@
-import React from 'react'
-import Conversation from './Conversation'
+import Conversation from "./Conversation";
+import useGetConversations from "../hooks/useGetConversations";
+import { getRandomEmoji } from "../utils/emojis";
+
 
 const Conversations = () => {
-    return (
-      <div className='py-2 flex flex-col overflow-auto'>
-      <Conversation/>
-      <Conversation/>     
-      <Conversation/>     
-      <Conversation/>
-      <Conversation/>     
-      <Conversation/>     
-      </div>
-    )
-  }
+	const { loading, conversations } = useGetConversations();
+	return (
+		<div className='py-2 flex flex-col overflow-auto'>
+			{conversations.map((conversation, idx) => (
+				<Conversation
+					key={conversation._id}
+					conversation={conversation}
+					emoji={getRandomEmoji()}
+					lastIdx={idx === conversations.length - 1}
+				/>
+			))}
 
+			{loading ? <span className='loading loading-spinner mx-auto'></span> : null}
+		</div>
+	);
+};
 export default Conversations;
-
-
-
 
 
 // STARTER CODE SNIPPET
@@ -29,11 +32,11 @@ export default Conversations;
 //     return (
 //       <div className='py-2 flex flex-col overflow-auto'>
 //       <Conversation/>
-//       <Conversation/>     
-//       <Conversation/>     
 //       <Conversation/>
-//       <Conversation/>     
-//       <Conversation/>     
+//       <Conversation/>
+//       <Conversation/>
+//       <Conversation/>
+//       <Conversation/>
 //       </div>
 //     )
 //   }
