4473ead5e387baee151bd94151e277e2fdc6cc69
venky1227 <46067605+venky1227@users.noreply.github.com>
2024-11-18 14:56:10 +0530

    Deployment

--- Backend/server.js
+++ Backend/server.js
@@ -34,7 +34,9 @@ app.get("*", (req, res) => {
 })
 
 
-
+// app.use((req, res) => {
+//     res.status(404).json({error: "Route not found"});
+// })
 
 server.listen(PORT, () => {
     connectToMongoDB();
