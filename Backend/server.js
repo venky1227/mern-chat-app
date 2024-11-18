ed8faea21aa5eb87d820692c1c91c58948ef4ff6
venky1227 <46067605+venky1227@users.noreply.github.com>
2024-11-18 14:24:46 +0530

    "Deployment"

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,7 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
+
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js"
 import messageRoutes from "./routes/message.routes.js"
@@ -13,20 +15,25 @@ import { app, server } from "./socket/socket.js";
 
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 dotenv.config();
 
 app.use(express.json()); 
 app.use(cookieParser());
 
+
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
+app.use(express.static(path.join(__dirname, "/Frontend/dist")))
 
-// app.get("/", (req, res) => {
-//     root route http://localhost:5000/
-//     res.send("Hello world!!");
-// });
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"),)
+})
+
+
 
 
 server.listen(PORT, () => {
